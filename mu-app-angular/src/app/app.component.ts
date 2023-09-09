3282dbebb82997147c04f30fcb2ae2da27f5c75c
borisgvgv <116513740+borisgvgv@users.noreply.github.com>
2023-09-09 18:16:01 +0000

    act

---
+++ mu-app-angular/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Hola mundo!</h1>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mu-app-angular';
+}
